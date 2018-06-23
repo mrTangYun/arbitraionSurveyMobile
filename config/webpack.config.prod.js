8bf3f867301166f11134e2cd0755e3acd6ed5948
唐云 <tangyun@speedto.cn>
2018-06-23 23:59:37 +0800

    为方便套数据，html不mini化

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -275,7 +275,7 @@ module.exports = {
       template: paths.appHtml,
       minify: {
         removeComments: true,
-        collapseWhitespace: true,
+        collapseWhitespace: false,
         removeRedundantAttributes: true,
         useShortDoctype: true,
         removeEmptyAttributes: true,
