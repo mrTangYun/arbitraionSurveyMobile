f3dd696e8881a4b130855c3733f8d4750f1501e3
唐云 <tangyun@speedto.cn>
2018-06-24 02:21:50 +0800

    适配pc

--- src/index.js
+++ src/index.js
@@ -10,6 +10,15 @@ import $ from 'jquery';
 const $btnPrev = $('.btn-pre'), $btnNext = $('.btn-next'), $btnSubmit = $('.btn-submit'), $prograssBar=$('.progress-bar');
 const questionCount = $('.questionItem').length;
 
+
+const isNeedPreventClickEnvent = (e) => {
+    if (e.type === 'click') {
+        return !this.isPc;
+    } else {
+        return this.isPc;
+    }
+};
+
 const calProgress = () => {
     const index = $('.questionItem:visible').index('.questionItem');
     const width = (index + 1) / questionCount * 100;
@@ -39,10 +48,16 @@ const bindHandler = () => {
         html.push('</span>');
         $(element).html(html.join(''));
     });
-    $(document).on('touchstart', '.answer.radios > div', {}, function() {
+    $(document).on('touchstart click', '.answer.radios > div', {}, function (e) {
+        if (isNeedPreventClickEnvent(e)) {
+            return false;
+        }
         $(this).addClass('choosed').siblings().removeClass('choosed');
     });
-    $(document).on('touchstart', '.answer.checkbox > div', {}, function() {
+    $(document).on('touchstart click', '.answer.checkbox > div', {}, function (e) {
+        if (isNeedPreventClickEnvent(e)) {
+            return false;
+        }
         if ($(this).hasClass('choosed')) {
             $(this).removeClass('choosed');
         }
@@ -51,24 +66,36 @@ const bindHandler = () => {
         }
     });
 
-    $(document).on('touchstart', '.btn-pre', {}, function() {
+    $(document).on('touchstart click', '.btn-pre', {}, (e) => {
+        if (isNeedPreventClickEnvent(e)) {
+            return false;
+        }
         const $currentItem = $('.questionItem:visible');
         $currentItem.hide().prev().show();
         calProgress();
     });
-    $(document).on('touchstart', '.btn-next', {}, function() {
+    $(document).on('touchstart click', '.btn-next', {}, (e) => {
+        if (isNeedPreventClickEnvent(e)) {
+            return false;
+        }
         const $currentItem = $('.questionItem:visible');
         $currentItem.hide().next().show();
         calProgress();
     });
 
-    $(document).on('touchstart', '.btn-start-chn', {}, function() {
+    $(document).on('touchstart click', '.btn-start-chn', {}, (e) => {
+        if (isNeedPreventClickEnvent(e)) {
+            return false;
+        }
         $('#homePage').addClass('hide');
         $('#questionPage').removeClass('hide');
         $('#changeLang').removeClass('langEn').addClass('langChn');
         calProgress();
     });
-    $(document).on('touchstart', '.btn-start-en', {}, function() {
+    $(document).on('touchstart click', '.btn-start-en', {}, (e) => {
+        if (isNeedPreventClickEnvent(e)) {
+            return false;
+        }
         $('#homePage').addClass('hide');
         $('#questionPage').removeClass('hide');
         $('#changeLang').removeClass('langChn').addClass('langEn');
@@ -78,7 +105,29 @@ const bindHandler = () => {
 };
 
 
+const IsPC = () => {
+    const userAgentInfo = navigator.userAgent;
+    const Agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+    let flag = true;
+    for (let v = 0; v < Agents.length; v++) {
+        if (userAgentInfo.indexOf(Agents[v]) > 0) { flag = false; break; }
+    }
+    if (flag) {
+        $('#root').addClass('pcStyle');
+    } else {
+        $('#root').removeClass('pcStyle');
+    }
+    return flag;
+};
+
 const init = () => {
+    const isPc = IsPC();
+    this.isPc = isPc;
+    $('img[data-pc]').map((index, item) => {
+        const $this = $(item);
+        const src = isPc ? $this.attr('data-pc') : $this.attr('data-mobile');
+        $this.attr('src', src);
+    });
     $('#loading').remove();
     $('.questionItem').hide().eq(0).show();
     bindHandler();
